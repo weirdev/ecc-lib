269eadedaa2c64c7f2222d048b4b2a505b8209ca
Wes Weirather <weirdev@outlook.com>
2017-12-27 00:13:19 -0600

    Implemented generating minimum polynomials

--- pyGF/gfmath.py
+++ pyGF/gfmath.py
@@ -13,32 +13,48 @@ class BinaryVector:
                 self.coeff = list(args[0].coeff)
             elif isinstance(args[0], int):
                 self.m = args[0]
-                self.coeff = [0] * self.m
+                self.coeff = [0] * (self.m + 1)
             else:
                 raise Exception()
         elif len(args) == 2:
             self.m = args[0]
             if isinstance(args[1], int):
-                self.coeff = inttobitlist(args[1])
-                self.coeff += [0]*(self.m-len(self.coeff))
+                self.coeff = inttobitlist(int(args[1]))
+                self.coeff += [0]*(self.m + 1 - len(self.coeff))
             else:
                 self.coeff = list(args[1])
-        
-    def __add__(self, other):
+
+    def addwithcarry(self, other):
         if isinstance(other, BinaryVector):
             if other.m != self.m:
                 raise Exception("Vectors must be of same size. Sizes {} and {}".format(self.m, other.m))
             carry = 0
             s = BinaryVector(self.m)
-            for i in range(self.m):
+            for i in range(self.m + 1):
+                ss = self.coeff[i] + other.coeff[i] + carry
+                s.coeff[i] = ss % 2
+                carry = 1 if ss >= 2 else 0
+            return s
+        else:
+            return self.addwithcarry(BinaryVector(self.m, other))
+        
+    def __add__(self, other):
+        if isinstance(other, BinaryVector):
+            if other.m > self.m:
+                raise Exception("Right vector must not be larger than left vector. Sizes {} and {}".format(self.m, other.m))
+            s = BinaryVector(self.m)
+            for i in range(other.m + 1):
                 s.coeff[i] = self.coeff[i] ^ other.coeff[i]
             return s
         else:
             return self + BinaryVector(self.m, other)
 
+    def __sub__(self, other):
+        return self + other
+
     def __mul__(self, other):
         if other.m != self.m:
-            raise Exception("Vectors must be of same size. Sizes {} and {}".format(self.m, other.m))
+            raise Exception("Vectors must be of same max order. Sizes {} and {}".format(self.m, other.m))
         modvect = BinaryVector(self.m + 1)
         modvect.coeff[0] = modvect.coeff[-1] = 1
         p = BinaryVector(self.m * 2)
@@ -49,32 +65,65 @@ class BinaryVector:
                 pp.coeff[i + j] = b1 & b2
             p += pp
             p %= modvect
-        return BinaryVector(self.m, p.coeff[:self.m])
+        return BinaryVector(self.m, p.coeff[:self.m+1])
 
     def __mod__(self, other):
-        if other.coeff[-1] == 0:
-            raise Exception("Leading coefficient cannot be zero.")
-        remainder = BinaryVector(self.m, self.coeff)
+        if other.iszero():
+            raise ZeroDivisionError()
+        other = BinaryVector(other)
+        retm = other.m
+        while other.coeff[-1] == 0:
+            other.m -= 1
+            other.coeff = other.coeff[:-1]
+        remainder = BinaryVector(self)
         if other.m > self.m:
             return remainder
-        pos = self.m        
-        while pos >= other.m:
+        pos = self.m + 1
+        while pos >= other.m + 1:
             if remainder.coeff[pos - 1] == 1:
-                diff = BinaryVector(other.m, remainder.coeff[pos-other.m:pos])
+                diff = BinaryVector(other.m, remainder.coeff[pos - other.m - 1:pos])
                 diff += other
-                remainder.coeff[pos-other.m:pos] = diff.coeff
+                remainder.coeff[pos - other.m - 1:pos] = diff.coeff
             pos -= 1
-        return remainder
+        return BinaryVector(retm, remainder.coeff[:retm+1])
+
+    def iszero(self):
+        return sum(self.coeff) == 0
+
+    @property
+    def degree(self):
+        for i in range(self.m):
+            if self.coeff[-i-1] != 0:
+                return self.m - i
+        return 0
+
+    def __rshift__(self, y):
+        shifted = BinaryVector(self)
+        if y != 0:
+            shifted.coeff[y:] = shifted.coeff[:-y]
+            shifted.coeff[:y] = [0]*y
+        return shifted
 
     def __eq__(self, other):
-        if other.m != self.m:
-            raise Exception("Fields must be of same size.")
-        return self.coeff == other.coeff
+        if isinstance(other, BinaryVector):
+            if other.m != self.m:
+                raise Exception("Fields must be of same size.")
+            return self.coeff == other.coeff
+        else:
+            return self == BinaryVector(self.m, other)
 
     def __str__(self):
         return str(self.coeff)
 
-def inttobitlist(x, bitlist=[]):
+    def __repr__(self):
+        return repr(self.coeff)
+
+    def __hash__(self):
+        return hash(tuple(self.coeff))
+
+def inttobitlist(x, bitlist=None):
+    if bitlist == None:
+        bitlist = list()
     if x == 0:
         if len(bitlist) > 0:
             return bitlist
@@ -83,9 +132,11 @@ def inttobitlist(x, bitlist=[]):
     return inttobitlist(x//2, bitlist)
 
 if __name__ == "__main__":
-    f1 = BinaryVector(3, [0, 0, 1])
-    f2 = BinaryVector(3, [1, 1, 0])
-    f3 = BinaryVector(3, [1, 1, 1, 0, 1, 1, 1, 0])
+    f1 = BinaryVector(2, [0, 0, 1])
+    f2 = BinaryVector(2, [1, 1, 0])
+    f3 = BinaryVector(2, [1, 1, 1, 0, 1, 1, 1, 0])
+    f4 = BinaryVector(f1)
+    print(f4)
     print(f3)
     print(f1 + f2)
     print(f1 * f2)
--- pyGF/primelement.py
+++ pyGF/primelement.py
@@ -1,28 +1,118 @@
 from gfmath import BinaryVector
 
+DEBUG = False
+
 def findprimelements(m):
     primeles = []
     e = BinaryVector(m, 2)
     i = 2
-    while i < m**2:
-        if isprimelement(e):
-            primeles.append(e)
-        e += 1
+    while i < 2**(m+1):
+        if is_primitive_element:
+                primeles.append(e)
         i += 1
+        e = e.addwithcarry(1)
     return primeles
 
-def isprimelement(e):
-    zero = BinaryVector(e.m)
+def is_primitive_element(e):
+    return irreducible(e) and divides_pow_x_n_m1(e)
+
+def divides_pow_x_n_m1(e):
     unity = BinaryVector(e.m, 1)
-    eorder = 2
-    print("e: {}".format(e))
-    ep = BinaryVector(e)
-    while ep != unity and ep != zero:
-        eorder += 1
-        ep *= e
-        print(ep)
-    return eorder == (2**e.m - 1)
+    checkorder = 1
+    remainder = unity
+
+    while checkorder < 2**e.m - 1:
+        ccp = BinaryVector(checkorder)
+        ccp.coeff[0] = ccp.coeff[-1] = 1
+        remainder = ccp % e
+        if DEBUG:
+            print("CO", checkorder)
+            print("Rem", remainder)
+            print("CCP", ccp)
+            print("E", e)
+        if remainder.iszero():
+            return False
+        checkorder += 1
+    ccp = BinaryVector(checkorder)
+    ccp.coeff[0] = ccp.coeff[-1] = 1
+    remainder = ccp % e
+    if DEBUG:
+        print("CO", checkorder)
+        print("Rem", remainder)
+        print("CCP", ccp)
+        print("E", e)
+    return remainder.iszero()
+
+def irreducible(e):
+    div = BinaryVector(e.degree, 2)
+    while div.degree < e.degree:
+        if (e % div).iszero():
+            return str(div), str(e % div)
+        div = div.addwithcarry(1)
+    return True
+
+def make_GF(primitive_polynomial):
+    m = primitive_polynomial.m
+    GF = []
+    GF.append(BinaryVector(m - 1))
+    for i in range(m):
+        GF.append(BinaryVector(m - 1, 2**i))
+    GF.append(BinaryVector(m-1, primitive_polynomial.coeff[:-1]))
+    for i in range(2**m-m-2):
+        powm = GF[-1].coeff[-1] == 1
+        new = GF[-1] >> 1
+        if powm:
+            new += GF[m+1]
+        GF.append(new)
+    return GF
+
+def minpoly(vector, gf):
+    conj = conjugates(vector, gf)
+    return polyproductmonicdeg1vectorpolys(conj, gf)
+
+def minpolyfactorvectterms(vector, gf):
+    return conjugates(vector, gf)
+
+def polyproductmonicdeg1vectorpolys(vectorterms, gf):
+    m = gf[0].m + 1
+    fieldorder = 2**m - 1
+    vectorpoly = [BinaryVector(m-1) for _ in range(len(vectorterms) + 1)]
+    vectorpoly[0] += 1
+    gfexpbyvect = {v: exp for exp, v in enumerate(gf[1:])}
+    # Vector poly = 1
+    # For each root, multiply vector poly by (root + x)
+    for root in vectorterms:
+        # Vector poly times root
+        vptr = []
+        for term in vectorpoly:
+            if not term.iszero():
+                vptr.append(gf[((gfexpbyvect[term] + gfexpbyvect[root]) % fieldorder) + 1])
+            else:
+                vptr.append(BinaryVector(m-1))
+        # Vector poly times x (the term we drop should always be zero, as the degree of vectorpoly increases by only 1 each iteration)
+        vptx = [BinaryVector(m-1)] + vectorpoly[:-1]
+        vectorpoly = [tr + tx for tr, tx in zip(vptr, vptx)]
+    for term in vectorpoly:
+        if not term.iszero() and term != 1:
+            raise Exception("Coefficients should come from GF(2)")
+    return BinaryVector(m, [1 if t==1 else 0 for t in vectorpoly])
+
+def conjugates(vector, gf):
+    m = vector.m + 1
+    fieldorder = 2**m - 1
+    vexp = gf.index(vector) - 1
+    conj = []
+    for _ in range(m):
+        conj.append(gf[vexp + 1])
+        vexp = (vexp * 2) % fieldorder
+    if gf[vexp + 1] == vector:
+        return conj
+    raise Exception("vector^(2^m) should == vector")
 
 if __name__ == "__main__":
-    for e in findprimelements(1):
-        print(e)
+    pp12 = BinaryVector(12, 7185)
+    pp5 = BinaryVector(5, 37)
+    gf5 = make_GF(pp5)
+    #print(conjugates(BinaryVector(4, [0,0,0,1,0]), gf5))
+    gf12 = make_GF(pp12)
+    print(minpolyfactorvectterms(BinaryVector(4, [0,0,0,1,0]), gf5))
